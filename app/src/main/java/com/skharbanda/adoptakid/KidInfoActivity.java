3d9b4793ed8017660ff7b626837fd6110ce64f6f
SSvtron <shadowtekg@gmail.com>
2020-02-23 06:36:50 -0800

    Improved UI

--- app/src/main/java/com/skharbanda/adoptakid/KidInfoActivity.java
+++ app/src/main/java/com/skharbanda/adoptakid/KidInfoActivity.java
@@ -1,8 +1,12 @@
 package com.skharbanda.adoptakid;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 
 public class KidInfoActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class KidInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kid_info);
+
+        // Define ActionBar object
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#bac3d4"));
+
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
+        actionBar.setTitle(Html.fromHtml("<font color='#ffffff'>AdoptAKid </font>"));
+        getSupportActionBar().setElevation(200);
     }
 }
--- app/src/main/java/com/skharbanda/adoptakid/MainActivity.java
+++ app/src/main/java/com/skharbanda/adoptakid/MainActivity.java
@@ -1,20 +1,26 @@
 package com.skharbanda.adoptakid;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -50,6 +56,30 @@ public class MainActivity extends AppCompatActivity {
 
         lView = findViewById(R.id.listview);
 
+        // Render Testing Button
+        Button buttonOne = findViewById(R.id.footerButton2);
+        buttonOne.setOnClickListener(v -> {
+            System.out.println("Button Clicked");
+
+            Intent activity2Intent = new Intent(getApplicationContext(), KidInfoActivity.class);
+            startActivity(activity2Intent);
+        });
+
+
+        // Define ActionBar object
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#bac3d4"));
+
+        // Set BackgroundDrawable
+        actionBar.setBackgroundDrawable(colorDrawable);
+        actionBar.setTitle(Html.fromHtml("<font color='#ffffff'>AdoptAKid </font>"));
+        getSupportActionBar().setElevation(20);
 
         // Access a Cloud Firestore instance from your Activity
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -68,8 +98,8 @@ public class MainActivity extends AppCompatActivity {
                                     String name = document.getString("name");
                                     String age = document.getString("age");
                                     String imageUrl = document.getString("image");
-                                    Toast.makeText(getApplicationContext(), "Name: " + name, Toast.LENGTH_SHORT).show();
-                                    Toast.makeText(getApplicationContext(), "Age: " + age, Toast.LENGTH_SHORT).show();
+                                    /*Toast.makeText(getApplicationContext(), "Name: " + name, Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(), "Age: " + age, Toast.LENGTH_SHORT).show();*/
 
                                     StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
                                     StrictMode.setThreadPolicy(policy);
@@ -81,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
                                     names.add(name);
                                     ages.add(age);
                                     images.add(d);
-                                    Toast.makeText(MainActivity.this, "Names 1 = " + names.toString(), Toast.LENGTH_SHORT).show();
+                                    /*Toast.makeText(MainActivity.this, "Names 1 = " + names.toString(), Toast.LENGTH_SHORT).show();*/
 
 
                                     String[] namesArr = new String[names.size()];
@@ -98,9 +128,9 @@ public class MainActivity extends AppCompatActivity {
                                     }
 
                                     lAdapter = new CustomLvAdapter(MainActivity.this, namesArr, agesArr, imagesArr);
-                                    Toast.makeText(MainActivity.this, "Names 2= " + namesArr, Toast.LENGTH_LONG).show();
+                                    /*Toast.makeText(MainActivity.this, "Names 2= " + namesArr, Toast.LENGTH_LONG).show();
                                     Toast.makeText(MainActivity.this, "Ages 2= " + agesArr, Toast.LENGTH_LONG).show();
-                                    Toast.makeText(MainActivity.this, "Images 2= " + imagesArr, Toast.LENGTH_LONG).show();
+                                    Toast.makeText(MainActivity.this, "Images 2= " + imagesArr, Toast.LENGTH_LONG).show();*/
 
                                     lView.setAdapter(lAdapter);
                                 } catch (Exception e) {
@@ -111,26 +141,7 @@ public class MainActivity extends AppCompatActivity {
                             Log.d("QueryDocumentSnapshot", "Error getting documents: ", task.getException());
                         }
                     }
-                });/*
-        String[] namesArr = new String[names.size()];
-        for(int i = 0; i < names.size(); i++) {
-            namesArr[i] = names.get(i);
-        }
-        String[] agesArr = new String[ages.size()];
-        for(int i = 0; i < ages.size(); i++) {
-            agesArr[i] = ages.get(i);
-        }
-        Drawable[] imagesArr = new Drawable[images.size()];
-        for(int i = 0; i < images.size(); i++) {
-            imagesArr[i] = images.get(i);
-        }
-
-        lAdapter = new CustomLvAdapter(MainActivity.this, namesArr, agesArr, imagesArr);
-        Toast.makeText(MainActivity.this, "Names 2= " + namesArr, Toast.LENGTH_LONG).show();
-        Toast.makeText(MainActivity.this, "Ages 2= " + agesArr, Toast.LENGTH_LONG).show();
-        Toast.makeText(MainActivity.this, "Images 2= " + imagesArr, Toast.LENGTH_LONG).show();
-
-        lView.setAdapter(lAdapter);*/
+                });
 
         lView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
