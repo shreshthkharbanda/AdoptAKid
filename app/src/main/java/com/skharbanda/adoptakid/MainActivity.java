89cfc48cd3f9acdf33818b196733298693386444
shreshthkharbanda <shreshthkharbanda1@gmail.com>
2020-02-22 20:16:49 -0800

    Initial commit

---
+++ app/src/main/java/com/skharbanda/adoptakid/MainActivity.java
@@ -0,0 +1,22 @@
+package com.skharbanda.adoptakid;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
