0ad51ef098c6247a6be3149449854e9663e14795
SSvtron <shadowtekg@gmail.com>
2020-03-17 22:27:04 -0700

    Added Signup XML

--- app/src/main/java/com/skharbanda/adoptakid/SingleShelterActivity.java
+++ app/src/main/java/com/skharbanda/adoptakid/SingleShelterActivity.java
@@ -1,25 +1,21 @@
 package com.skharbanda.adoptakid;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SingleShelterActivity extends AppCompatActivity {
 
